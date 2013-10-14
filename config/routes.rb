6d8e4f485a4b5ce02d95fbc012e6caa3433c4389
Nejuf <matthew.b.blankenship@gmail.com>
2013-10-14 13:09:52 -0700

    Adds routes for bands, albums, tracks

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,16 @@ MusicApp::Application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
 
-  resources :bands
+  resources :bands do
+    resources :albums, only: [:index, :new, :create]
+  end
 
+  resources :albums, except: [:index, :new, :create] do
+    resources :tracks, only: [:index, :new, :create]
+  end
+
+  resources :tracks, except: [:index, :new, :create]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
